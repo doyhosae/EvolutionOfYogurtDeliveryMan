af22631babf66d55e0b0ab6608c86571f11f6e09
doyhosae <doyhosae@gmail.com>
2021-02-09 19:25:09 +0900

    update 2.0

--- main.js
+++ main.js
@@ -1,42 +1,43 @@
 let currentScene;
 
-let level0 = "img/000.png";
-let level1 = "img/001.png";
-let level2 = "img/002.png";
-let level3 = "img/003.png";
-let mainImage__URL = document.getElementById("mainImage");
-let title__JS = document.getElementById("title");
+const level0 = "img/000.png";
+const level1 = "img/001.png";
+const level2 = "img/002.png";
+const level3 = "img/003.png";
+const mainImage__URL = document.getElementById("mainImage");
+const title__JS = document.getElementById("title");
+const btn = document.getElementById('btn');
 
 
 function drawScene1(){
     currentScene = 1;
     document.body.style.backgroundColor = "rgb(199, 182, 121)";
-    title__JS.textContent = "Level 0";
+    title__JS.textContent = "●○○○";
     mainImage__URL.src = level0;
 }
 
 function drawScene2(){
     currentScene = 2;
     document.body.style.backgroundColor = "rgb(199, 192, 121)";
-    title__JS.textContent = "Level 1";
+    title__JS.textContent = "●●○○";
     mainImage__URL.src = level1;
 }
 
 function drawScene3(){
     currentScene = 3;
     document.body.style.backgroundColor = "rgb(199, 202, 121)";
-    title__JS.textContent = "Level 2";
+    title__JS.textContent = "●●●○";
     mainImage__URL.src = level2;
 }
 
 function drawScene4(){
     currentScene = 4;
     document.body.style.backgroundColor = "rgb(199, 212, 121)";
-    title__JS.textContent = "Level 3(Final)🦍";
+    title__JS.textContent = "🦍🦍🦍🦍";
     mainImage__URL.src = level3;
 }
 
-onclick = function(event){
+let onButtonClick = function() {
     if (currentScene === 1){
         drawScene2();
     } else if (currentScene === 2){
@@ -46,7 +47,11 @@ onclick = function(event){
     } else if (currentScene === 4){
         drawScene1();
     }
+  };
+
+if (btn) {
+    btn.addEventListener("click", onButtonClick)
 }
 
-//main
+// main
 drawScene1();
